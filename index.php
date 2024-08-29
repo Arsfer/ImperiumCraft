33e71c45beca1826866a76b815ea631ab5f39e50
Gleb V <valievgv@yandex.ru>
2024-08-29 19:02:57 +0300

    (add) .gitignore

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ImperiumCraft - Сайт в разработке</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="icon" href="./public/imperiumcraftfav.png" type="image/png">
+</head>
+<body>
+<div class="content">
+    <h1>Сайт в разработке</h1>
+    <p>Добро пожаловать на ImperiumCraft. Наш сайт скоро будет доступен.</p>
+    <p class="datetime">Текущая дата и время: <?php echo date('d.m.Y H:i:s'); ?></p>
+    <p class="php-status"><?php echo "PHP успешно работает на этом сервере."; ?></p>
+</div>
+</body>
+</html>
